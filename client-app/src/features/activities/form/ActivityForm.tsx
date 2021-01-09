f8edffef87773a217f550405d5ad41aa07349e54
DanielIgnatov <danielignatov@outlook.com>
2021-01-09 19:11:40 +0200

    design makeover

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { FormEvent } from 'react';
-import { Button, Form, Segment } from 'semantic-ui-react';
+import { Button, Form, Grid, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import { observer } from 'mobx-react-lite';
@@ -11,17 +11,17 @@ interface DetailParams {
     id: string;
 }
 
-const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, history}) => {
+const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, history }) => {
     const activityStore = useContext(ActivityStore);
-    const { 
-        createActivity, 
-        editActivity, 
-        submitting, 
-        activity: initialFormState, 
-        loadActivity, 
-        clearActivity 
+    const {
+        createActivity,
+        editActivity,
+        submitting,
+        activity: initialFormState,
+        loadActivity,
+        clearActivity
     } = activityStore;
-    
+
     const [activity, setActivity] = useState<IActivity>({
         id: '',
         title: '',
@@ -60,23 +60,28 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({match, histo
     }
 
     return (
-        <Segment clearing={true}>
-            <Form onSubmit={handleSubmit} >
-                <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title} />
-                <Form.TextArea rows={2} onChange={handleInputChange} name='description' placeholder='Description' value={activity.description} />
-                <Form.Input onChange={handleInputChange} name='category' placeholder='Category' value={activity.category} />
-                <Form.Input
-                    onChange={handleInputChange}
-                    name='date'
-                    type='datetime-local'
-                    placeholder='Date'
-                    value={activity.date} />
-                <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
-                <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
-                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
-                <Button onClick={() => history.push('/activities')} floated='right' type='submit' content='Cancel' />
-            </Form>
-        </Segment>
+        <Grid>
+            <Grid.Column width={10}>
+                <Segment clearing={true}>
+                    <Form onSubmit={handleSubmit} >
+                        <Form.Input onChange={handleInputChange} name='title' placeholder='Title' value={activity.title} />
+                        <Form.TextArea rows={2} onChange={handleInputChange} name='description' placeholder='Description' value={activity.description} />
+                        <Form.Input onChange={handleInputChange} name='category' placeholder='Category' value={activity.category} />
+                        <Form.Input
+                            onChange={handleInputChange}
+                            name='date'
+                            type='datetime-local'
+                            placeholder='Date'
+                            value={activity.date} />
+                        <Form.Input onChange={handleInputChange} name='city' placeholder='City' value={activity.city} />
+                        <Form.Input onChange={handleInputChange} name='venue' placeholder='Venue' value={activity.venue} />
+                        <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
+                        <Button onClick={() => history.push('/activities')} floated='right' type='submit' content='Cancel' />
+                    </Form>
+                </Segment>
+            </Grid.Column>
+        </Grid>
+
     )
 }
 
