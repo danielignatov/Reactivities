850aba402c0b4b902648793a9a86e9b0ef66acf2
DanielIgnatov <danielignatov@outlook.com>
2021-03-13 22:45:44 +0200

    Added date time pickers

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -19,11 +19,11 @@ class ActivityStore {
 
     groupActivitiesByDate(activities: IActivity[]) {
         const sortedActivities = activities.sort(
-            (a, b) => a.date!.getTime() - b.date!.getTime()
+            (a, b) => a.date.getTime() - b.date.getTime()
         )
 
         return Object.entries(sortedActivities.reduce((activities, activity) => {
-            const date = activity.date!.toISOString().split('T')[0];
+            const date = activity.date.toISOString().split('T')[0];
 
             activities[date]  = activities[date] ? [...activities[date], activity] : [activity];
 
@@ -42,7 +42,7 @@ class ActivityStore {
             const activities = await agent.Activities.list();
             runInAction(() => {
                 activities.forEach((activity) => {
-                    activity.date = new Date(activity.date!);
+                    activity.date = new Date(activity.date);
                     this.activityRegistry.set(activity.id, activity);
                 });
             });
@@ -61,6 +61,7 @@ class ActivityStore {
 
         if (activity) {
             this.activity = activity;
+            return activity;
         } else {
             this.loadingInitial = true;
 
@@ -69,7 +70,10 @@ class ActivityStore {
                 runInAction(() => {
                     activity.date = new Date(activity.date);
                     this.activity = activity;
+                    this.activityRegistry.set(activity.id, activity);
+                    this.loadingInitial = false;
                 })
+                return activity;
             } catch (error) {
                 console.error(error);
             } finally {
@@ -95,6 +99,7 @@ class ActivityStore {
             runInAction(() => {
                 this.activityRegistry.set(activity.id, activity);
             });
+            //history.push(`/activities/${activity.id}`)
         } catch (error) {
             console.error(error);
         } finally {
@@ -112,6 +117,7 @@ class ActivityStore {
                 this.activityRegistry.set(activity.id, activity);
                 this.activity = activity;
             });
+            //history.push(`/activities/${activity.id}`)
         } catch (error) {
             console.error(error);
         } finally {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState } from 'react';
 //import { FormEvent } from 'react';
 import { Button, Form, Grid, Segment } from 'semantic-ui-react';
-import { IActivity } from '../../../app/models/activity';
-//import { v4 as uuid } from 'uuid';
+import { ActivityFormValues, IActivityFormValues } from '../../../app/models/activity';
+import { v4 as uuid } from 'uuid';
 import { observer } from 'mobx-react-lite';
 import ActivityStore from '../../../app/stores/activityStore';
 import { RouteComponentProps } from 'react-router-dom';
@@ -12,6 +12,7 @@ import TextAreaInput from '../../../app/common/form/TextAreaInput';
 import SelectInput from '../../../app/common/form/SelectInput';
 import { category } from '../../../app/common/options/categoryOptions';
 import DateInput from '../../../app/common/form/DateInput';
+import { combineDateAndTime } from '../../../app/common/util/util';
 
 interface DetailParams {
     id: string;
@@ -20,46 +21,49 @@ interface DetailParams {
 const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, history }) => {
     const activityStore = useContext(ActivityStore);
     const {
-        //createActivity,
-        //editActivity,
+        createActivity,
+        editActivity,
         submitting,
-        activity: initialFormState,
-        loadActivity,
-        clearActivity
+        //activity: initialFormState,
+        loadActivity
     } = activityStore;
 
-    const [activity, setActivity] = useState<IActivity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<IActivityFormValues>(new ActivityFormValues());
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        if (match.params.id && activity.id.length === 0) {
-            loadActivity(match.params.id).then(
-                () => initialFormState && setActivity(initialFormState));
+        if (match.params.id) {
+            setLoading(true);
+            loadActivity(match.params.id)
+            .then((activity) => setActivity(new ActivityFormValues(activity)))
+            .finally(() => setLoading(false));
         }
-        return () => {
-            clearActivity();
-        }
-    }, [loadActivity, match.params.id, clearActivity, initialFormState, activity.id.length])
+    }, [loadActivity, match.params.id])
 
     const handleFinalFormSubmit = (values: any) => {
-        console.log(values);
+        const dateAndTime = combineDateAndTime(values.date, values.time);
+        const {date, time, ...activity} = values;
+        activity.date = dateAndTime;
+        if (!activity.id) {
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity);
+        } else {
+            editActivity(activity);
+        }
     }
 
     return (
         <Grid>
             <Grid.Column width={10}>
-                <Segment clearing={true}>
+                <Segment clearing>
                     <FinalForm
+                        initialValues={activity}
                         onSubmit={handleFinalFormSubmit}
                         render={({ handleSubmit }) => (
-                            <Form onSubmit={handleSubmit} >
+                            <Form onSubmit={handleSubmit} loading={loading} >
                                 <Field
                                     component={TextInput}
                                     name='title'
@@ -83,13 +87,13 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, hist
                                         name='date'
                                         date={true}
                                         placeholder='Date'
-                                        value={activity.date!} />
+                                        value={activity.date} />
                                     <Field
                                         component={DateInput}
-                                        name='date'
+                                        name='time'
                                         time={true}
                                         placeholder='Time'
-                                        value={activity.date!} />
+                                        value={activity.time} />
                                 </Form.Group>
                                 <Field
                                     component={TextInput}
@@ -101,8 +105,23 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({ match, hist
                                     name='venue'
                                     placeholder='Venue'
                                     value={activity.venue} />
-                                <Button loading={submitting} floated='right' positive type='submit' content='Submit' />
-                                <Button onClick={() => history.push('/activities')} floated='right' type='submit' content='Cancel' />
+                                <Button 
+                                disabled={loading} 
+                                loading={submitting} 
+                                floated='right' 
+                                positive type='submit' 
+                                content='Submit' />
+                                <Button 
+                                disabled={loading} 
+                                onClick={e => {
+                                    e.preventDefault();
+                                    activity.id
+                                      ? history.push(`/activities/${activity.id}`)
+                                      : history.push('/activities');
+                                  }}
+                                floated='right' 
+                                type='text' 
+                                content='Cancel' />
                             </Form>
                         )}
                     />
